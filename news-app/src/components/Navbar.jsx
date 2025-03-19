01484fc6dd32f7754d098f134dc27b85370c8ffc
hamza errahmouni <maradox6ken@gmail.com>
2025-03-19 20:45:50 +0000

    Add logo to navbar

--- news-app/src/components/Navbar.jsx
+++ news-app/src/components/Navbar.jsx
@@ -17,9 +17,24 @@ const Navbar = () => {
     },
   ];
   return (
+    /*<header>
+      <nav className="flex items-center justify-between p-4 bg-black">
+        <a href="/">
+          <img src={alxnewsLogo} alt="ALX News Logo" className="h-12 w-auto" />
+        </a>
+      </nav>
+    </header>*/
+
     <header className="bg-black">
-      {/*hearder nav logo Alx news*/}
-      <nav className="px-4 py-4"></nav>
+      {/*logo Alx news*/}
+      <nav className="px-4 py-4">
+        <a href="/" className="text-xl font-baold text-white">
+          Alx
+          <span className="text-red-400">
+            <span className="text-blue-500">N</span>EWS
+          </span>
+        </a>
+      </nav>
     </header>
   );
 };
