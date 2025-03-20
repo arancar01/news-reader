6cad6bb1a449beea79dc297d8cb7c936ed706589
hamza errahmouni <maradox6ken@gmail.com>
2025-03-20 00:18:19 +0000

    Implemented dynamic navigation menu with NavLink

--- news-app/src/components/Navbar.jsx
+++ news-app/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 //import alxnewsLogo from "../assets/alxnews.png";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const navItems = [
@@ -27,13 +28,46 @@ const Navbar = () => {
 
     <header className="bg-black">
       {/*logo Alx news*/}
-      <nav className="px-4 py-4">
-        <a href="/" className="text-xl font-baold text-white">
+      <nav className="flex items-center justify-between px-6 py-4">
+        <a href="/" className="text-xl font-bold text-white">
           Alx
           <span className="text-red-400">
             <span className="text-blue-500">N</span>EWS
           </span>
         </a>
+        {/*navItems */}
+        {/*
+          <ul className="flex items-center justify-center gap-6 text-lg font-medium text-white">
+            <li>
+              <a href="/" className="hover:text-blue-500 transition">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/about" className="hover:text-blue-500 transition">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/register" className="hover:text-blue-500 transition">
+                Register
+              </a>
+            </li>
+          </ul>
+        */}
+
+        <ul className="flex items-center justify-center gap-6">
+          {navItems.map(({ path, link }) => (
+            <li key={path}>
+              <NavLink
+                to={path}
+                className="text-white hover:text-blue-500 transition"
+              >
+                {link}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
