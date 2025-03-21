e4f079b278e1d0d34aae8970005556ec8136499d
hamza errahmouni <maradox6ken@gmail.com>
2025-03-21 18:31:54 +0000

    refactor: use Formik for form state

--- news-app/src/pages/Signin.jsx
+++ news-app/src/pages/Signin.jsx
@@ -1,26 +1,52 @@
 import React from "react";
+import { useFormik } from "formik";
 
 const Signin = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <form className="bg-white p-6 rounded shadow-md w-full max-w-md">
-        <h2 className="text-xl font-bold mb-4">Create Account</h2>
+    <div className="min-h-screen flex items-center bg-gray-100 justify-center">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="shadow p-6 rounded w-full max-w-md bg-white"
+      >
+        <h2 className="mb-4 text-xl font-bold">Create Account</h2>
         <input
           type="text"
+          name="username"
           placeholder="Username"
-          className="border px-3 py-2 rounded w-full mb-3"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          className="w-full border px-3 py-2 rounded mb-3"
         />
         <input
           type="email"
+          name="email"
           placeholder="Email"
-          className="border px-3 py-2 rounded w-full mb-3"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          className="border w-full py-2 px-3 rounded mb-3"
         />
         <input
           type="password"
+          name="password"
           placeholder="Password"
-          className="border px-3 py-2 rounded w-full mb-4"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          className="px-3 py-2 mb-4 w-full border rounded"
         />
-        <button className="w-full bg-green-300 text-white py-2 rounded">
+        <button
+          type="submit"
+          className="text-white bg-green-500 rounded py-2 w-full hover:bg-green-600"
+        >
           Sign In
         </button>
       </form>
