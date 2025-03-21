0efc185e8816b4447f327c05895b4a25b38923c2
hamza errahmouni <maradox6ken@gmail.com>
2025-03-21 18:09:01 +0000

    add login image on left side of the page

--- news-app/src/pages/Login.jsx
+++ news-app/src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useFormik } from "formik";
 import { useNavigate, Link } from "react-router-dom";
+import loginImage from "../assets/login.png";
 
 const Login = () => {
   const navigate = useNavigate();
+
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -11,6 +13,7 @@ const Login = () => {
     },
     onSubmit: (values) => {
       const user = JSON.parse(localStorage.getItem("user"));
+
       if (
         user &&
         user.email === values.email &&
@@ -23,43 +26,54 @@ const Login = () => {
     },
   });
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <form
-        onSubmit={formik.handleSubmit}
-        className="bg-white p-6 rounded shadow-md w-full max-w-md"
-      >
-        <h2 className="text-xl font-bold mb-4">Login</h2>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          className="border px-3 py-2 w-full mb-3"
+    <div className="min-h-screen flex">
+      {/* Image */}
+      <div className="w-1/2 hidden md:block">
+        <img
+          src={loginImage}
+          alt="Login"
+          className="h-full w-full object-cover"
         />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          className="border px-3 py-2 w-full mb-4"
-        />
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 rounded"
+      </div>
+      {/* form  */}
+      <div className="w-full md:w-1/2 flex items-center justify-center bg-gray-100">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="bg-white p-6 rounded shadow-md w-full max-w-md"
         >
-          Login
-        </button>
-        <Link to="/signin">
+          <h2 className="text-xl font-bold mb-4">Login</h2>
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            className="border px-3 py-2 w-full mb-3"
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            className="border px-3 py-2 w-full mb-4"
+          />
           <button
-            type="button"
-            className="w-full mt-3 bg-gray-500 text-white py-2 rounded"
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded"
           >
-            Create Account
+            Login
           </button>
-        </Link>
-      </form>
+          <Link to="/signin">
+            <button
+              type="button"
+              className="w-full mt-3 bg-gray-500 text-white py-2 rounded"
+            >
+              Create Account
+            </button>
+          </Link>
+        </form>
+      </div>
     </div>
   );
 };
