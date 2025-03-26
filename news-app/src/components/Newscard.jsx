80e890f341537195e99d0f825651c523e989d92a
hamza errahmouni <maradox6ken@gmail.com>
2025-03-26 23:37:48 +0000

    accept article as prop and render its data

--- news-app/src/components/Newscard.jsx
+++ news-app/src/components/Newscard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Newscard = () => {
+const Newscard = ({ article }) => {
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden">
       <div className="p-4">
-        <h2 className="text-lg font-bold text-gray-800">Title</h2>
-        <p className="text-sm text-gray-500 mb-2">Date</p>
-        <p className="text-gray-700">Description</p>
+        <h2 className="text-lg font-bold text-gray-800">{article.title}</h2>
+        <p className="text-sm text-gray-500 mb-2">{article.publishedAt}</p>
+        <p className="text-gray-700">{article.description}</p>
       </div>
     </div>
   );
